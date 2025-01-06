abb40ef55fd26ef1b3ec9d7f3916b05ca8e9c2ce
rifedothman <rifed.othmen@gmail.com>
2025-01-06 17:43:36 +0100

    first one

---
+++ swipe-network/src/main/java/com/rifed/swipe_network/SwipeNetworkApplication.java
@@ -0,0 +1,13 @@
+package com.rifed.swipe_network;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipeNetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipeNetworkApplication.class, args);
+	}
+
+}
---
+++ swipe-network/src/test/java/com/rifed/swipe_network/SwipeNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rifed.swipe_network;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwipeNetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
