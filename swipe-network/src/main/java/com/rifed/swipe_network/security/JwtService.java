6b4b39022b95c2f00c75536227e4f45e353f63c4
rifedothman <rifed.othmen@gmail.com>
2025-01-08 18:54:56 +0100

    jwt filter

---
+++ swipe-network/src/main/java/com/rifed/swipe_network/security/JwtService.java
@@ -0,0 +1,12 @@
+package com.rifed.swipe_network.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+
+}
+
